aafc67cab29e8b7b87589f047c2ad38a98362ca7
Undying Soul <UndyingSoul.mc@gmail.com>
2020-02-27 22:53:09 -0700

    Add project files.

---
+++ Project JAPA/Form1.Designer.cs
@@ -0,0 +1,345 @@
+﻿namespace Project_JAPA
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.statusBar = new System.Windows.Forms.ToolStripStatusLabel();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.startJAPAToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator3 = new System.Windows.Forms.ToolStripSeparator();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.usingJAPAToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.configuringJAPAToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.troubleshootingToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.manualToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.viewToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.commandsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.logToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.logUserInteractionsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.setLogDirectoryToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator4 = new System.Windows.Forms.ToolStripSeparator();
+            this.viewLogsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.settingsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.textModeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.silentModeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.startAutomaticallyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.hideWhenMinimizedToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.resetCommandsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.modifyCommandsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.userInput = new System.Windows.Forms.TextBox();
+            this.lbl_command = new System.Windows.Forms.Label();
+            this.tb_console = new System.Windows.Forms.RichTextBox();
+            this.btn_submitCommand = new System.Windows.Forms.Button();
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.statusStrip1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // statusStrip1
+            // 
+            resources.ApplyResources(this.statusStrip1, "statusStrip1");
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.statusBar});
+            this.statusStrip1.Name = "statusStrip1";
+            // 
+            // statusBar
+            // 
+            resources.ApplyResources(this.statusBar, "statusBar");
+            this.statusBar.Name = "statusBar";
+            // 
+            // menuStrip1
+            // 
+            resources.ApplyResources(this.menuStrip1, "menuStrip1");
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.helpToolStripMenuItem,
+            this.viewToolStripMenuItem});
+            this.menuStrip1.Name = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            resources.ApplyResources(this.fileToolStripMenuItem, "fileToolStripMenuItem");
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.startJAPAToolStripMenuItem,
+            this.toolStripSeparator3,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            // 
+            // startJAPAToolStripMenuItem
+            // 
+            resources.ApplyResources(this.startJAPAToolStripMenuItem, "startJAPAToolStripMenuItem");
+            this.startJAPAToolStripMenuItem.Name = "startJAPAToolStripMenuItem";
+            this.startJAPAToolStripMenuItem.Click += new System.EventHandler(this.startJAPAToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator3
+            // 
+            resources.ApplyResources(this.toolStripSeparator3, "toolStripSeparator3");
+            this.toolStripSeparator3.Name = "toolStripSeparator3";
+            // 
+            // exitToolStripMenuItem
+            // 
+            resources.ApplyResources(this.exitToolStripMenuItem, "exitToolStripMenuItem");
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // helpToolStripMenuItem
+            // 
+            resources.ApplyResources(this.helpToolStripMenuItem, "helpToolStripMenuItem");
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.usingJAPAToolStripMenuItem,
+            this.configuringJAPAToolStripMenuItem,
+            this.troubleshootingToolStripMenuItem,
+            this.manualToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            // 
+            // usingJAPAToolStripMenuItem
+            // 
+            resources.ApplyResources(this.usingJAPAToolStripMenuItem, "usingJAPAToolStripMenuItem");
+            this.usingJAPAToolStripMenuItem.Name = "usingJAPAToolStripMenuItem";
+            // 
+            // configuringJAPAToolStripMenuItem
+            // 
+            resources.ApplyResources(this.configuringJAPAToolStripMenuItem, "configuringJAPAToolStripMenuItem");
+            this.configuringJAPAToolStripMenuItem.Name = "configuringJAPAToolStripMenuItem";
+            // 
+            // troubleshootingToolStripMenuItem
+            // 
+            resources.ApplyResources(this.troubleshootingToolStripMenuItem, "troubleshootingToolStripMenuItem");
+            this.troubleshootingToolStripMenuItem.Name = "troubleshootingToolStripMenuItem";
+            // 
+            // manualToolStripMenuItem
+            // 
+            resources.ApplyResources(this.manualToolStripMenuItem, "manualToolStripMenuItem");
+            this.manualToolStripMenuItem.Name = "manualToolStripMenuItem";
+            // 
+            // viewToolStripMenuItem
+            // 
+            resources.ApplyResources(this.viewToolStripMenuItem, "viewToolStripMenuItem");
+            this.viewToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.commandsToolStripMenuItem,
+            this.logToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.settingsToolStripMenuItem});
+            this.viewToolStripMenuItem.Name = "viewToolStripMenuItem";
+            // 
+            // commandsToolStripMenuItem
+            // 
+            resources.ApplyResources(this.commandsToolStripMenuItem, "commandsToolStripMenuItem");
+            this.commandsToolStripMenuItem.Name = "commandsToolStripMenuItem";
+            // 
+            // logToolStripMenuItem
+            // 
+            resources.ApplyResources(this.logToolStripMenuItem, "logToolStripMenuItem");
+            this.logToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.logUserInteractionsToolStripMenuItem,
+            this.setLogDirectoryToolStripMenuItem,
+            this.toolStripSeparator4,
+            this.viewLogsToolStripMenuItem});
+            this.logToolStripMenuItem.Name = "logToolStripMenuItem";
+            // 
+            // logUserInteractionsToolStripMenuItem
+            // 
+            resources.ApplyResources(this.logUserInteractionsToolStripMenuItem, "logUserInteractionsToolStripMenuItem");
+            this.logUserInteractionsToolStripMenuItem.Name = "logUserInteractionsToolStripMenuItem";
+            // 
+            // setLogDirectoryToolStripMenuItem
+            // 
+            resources.ApplyResources(this.setLogDirectoryToolStripMenuItem, "setLogDirectoryToolStripMenuItem");
+            this.setLogDirectoryToolStripMenuItem.Name = "setLogDirectoryToolStripMenuItem";
+            // 
+            // toolStripSeparator4
+            // 
+            resources.ApplyResources(this.toolStripSeparator4, "toolStripSeparator4");
+            this.toolStripSeparator4.Name = "toolStripSeparator4";
+            // 
+            // viewLogsToolStripMenuItem
+            // 
+            resources.ApplyResources(this.viewLogsToolStripMenuItem, "viewLogsToolStripMenuItem");
+            this.viewLogsToolStripMenuItem.Name = "viewLogsToolStripMenuItem";
+            // 
+            // toolStripSeparator1
+            // 
+            resources.ApplyResources(this.toolStripSeparator1, "toolStripSeparator1");
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            // 
+            // settingsToolStripMenuItem
+            // 
+            resources.ApplyResources(this.settingsToolStripMenuItem, "settingsToolStripMenuItem");
+            this.settingsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.textModeToolStripMenuItem,
+            this.silentModeToolStripMenuItem,
+            this.startAutomaticallyToolStripMenuItem,
+            this.hideWhenMinimizedToolStripMenuItem,
+            this.toolStripSeparator2,
+            this.resetCommandsToolStripMenuItem,
+            this.modifyCommandsToolStripMenuItem});
+            this.settingsToolStripMenuItem.Name = "settingsToolStripMenuItem";
+            // 
+            // textModeToolStripMenuItem
+            // 
+            resources.ApplyResources(this.textModeToolStripMenuItem, "textModeToolStripMenuItem");
+            this.textModeToolStripMenuItem.Name = "textModeToolStripMenuItem";
+            this.textModeToolStripMenuItem.Click += new System.EventHandler(this.textModeToolStripMenuItem_Click);
+            // 
+            // silentModeToolStripMenuItem
+            // 
+            resources.ApplyResources(this.silentModeToolStripMenuItem, "silentModeToolStripMenuItem");
+            this.silentModeToolStripMenuItem.Name = "silentModeToolStripMenuItem";
+            this.silentModeToolStripMenuItem.Click += new System.EventHandler(this.silentModeToolStripMenuItem_Click);
+            // 
+            // startAutomaticallyToolStripMenuItem
+            // 
+            resources.ApplyResources(this.startAutomaticallyToolStripMenuItem, "startAutomaticallyToolStripMenuItem");
+            this.startAutomaticallyToolStripMenuItem.Name = "startAutomaticallyToolStripMenuItem";
+            this.startAutomaticallyToolStripMenuItem.Click += new System.EventHandler(this.startAutomaticallyToolStripMenuItem_Click);
+            // 
+            // hideWhenMinimizedToolStripMenuItem
+            // 
+            resources.ApplyResources(this.hideWhenMinimizedToolStripMenuItem, "hideWhenMinimizedToolStripMenuItem");
+            this.hideWhenMinimizedToolStripMenuItem.Name = "hideWhenMinimizedToolStripMenuItem";
+            // 
+            // toolStripSeparator2
+            // 
+            resources.ApplyResources(this.toolStripSeparator2, "toolStripSeparator2");
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            // 
+            // resetCommandsToolStripMenuItem
+            // 
+            resources.ApplyResources(this.resetCommandsToolStripMenuItem, "resetCommandsToolStripMenuItem");
+            this.resetCommandsToolStripMenuItem.Name = "resetCommandsToolStripMenuItem";
+            this.resetCommandsToolStripMenuItem.Click += new System.EventHandler(this.resetConfigurationToolStripMenuItem_Click);
+            // 
+            // modifyCommandsToolStripMenuItem
+            // 
+            resources.ApplyResources(this.modifyCommandsToolStripMenuItem, "modifyCommandsToolStripMenuItem");
+            this.modifyCommandsToolStripMenuItem.Name = "modifyCommandsToolStripMenuItem";
+            // 
+            // userInput
+            // 
+            resources.ApplyResources(this.userInput, "userInput");
+            this.userInput.Name = "userInput";
+            this.userInput.TextChanged += new System.EventHandler(this.userInput_TextChanged);
+            // 
+            // lbl_command
+            // 
+            resources.ApplyResources(this.lbl_command, "lbl_command");
+            this.lbl_command.Name = "lbl_command";
+            // 
+            // tb_console
+            // 
+            resources.ApplyResources(this.tb_console, "tb_console");
+            this.tb_console.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tb_console.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.tb_console.Name = "tb_console";
+            this.tb_console.ReadOnly = true;
+            this.tb_console.ShortcutsEnabled = false;
+            this.tb_console.TabStop = false;
+            this.tb_console.TextChanged += new System.EventHandler(this.tb_console_TextChanged);
+            // 
+            // btn_submitCommand
+            // 
+            resources.ApplyResources(this.btn_submitCommand, "btn_submitCommand");
+            this.btn_submitCommand.Name = "btn_submitCommand";
+            this.btn_submitCommand.UseVisualStyleBackColor = true;
+            this.btn_submitCommand.Click += new System.EventHandler(this.btn_submitCommand_Click);
+            // 
+            // notifyIcon
+            // 
+            this.notifyIcon.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Error;
+            resources.ApplyResources(this.notifyIcon, "notifyIcon");
+            this.notifyIcon.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon_MouseClick);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btn_submitCommand;
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.btn_submitCommand);
+            this.Controls.Add(this.tb_console);
+            this.Controls.Add(this.lbl_command);
+            this.Controls.Add(this.userInput);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel statusBar;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem viewToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem commandsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem settingsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem silentModeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem textModeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem startAutomaticallyToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripMenuItem modifyCommandsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem usingJAPAToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem configuringJAPAToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem troubleshootingToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem manualToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem resetCommandsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem startJAPAToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator3;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem hideWhenMinimizedToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem logToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem logUserInteractionsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem setLogDirectoryToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator4;
+        private System.Windows.Forms.ToolStripMenuItem viewLogsToolStripMenuItem;
+        private System.Windows.Forms.TextBox userInput;
+        private System.Windows.Forms.Label lbl_command;
+        private System.Windows.Forms.RichTextBox tb_console;
+        private System.Windows.Forms.Button btn_submitCommand;
+        private System.Windows.Forms.NotifyIcon notifyIcon;
+    }
+}
+
---
+++ Project JAPA/Form1.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NLog;
+using NLog.Windows.Forms;
+using System.Speech.Recognition;
+using System.Speech.Synthesis;
+using AIMLbot;
+
+namespace Project_JAPA
+{
+	public partial class Form1 : Form
+	{
+		#region properties
+		public bool _JAPAenabled = false;
+		public bool _JAPArunning = false;
+		public bool _expectsResponse = false;
+		public List<string> conversation = new List<string>();
+		public static Logger logger = LogManager.GetCurrentClassLogger();
+		Bot companion = new Bot();
+		User user;
+		SpeechRecognitionEngine recognitionEngine;
+		#endregion
+
+        public Form1()
+		{
+			InitializeComponent();
+			this.AcceptButton = btn_submitCommand;
+			companion.loadSettings();
+			companion.loadAIMLFromFiles();
+			companion.isAcceptingUserInput = false;
+			user = new User(Environment.UserName, companion);
+		}
+
+		private void resetConfigurationToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			// TODO - need to do this.
+		}
+
+		private void silentModeToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			silentModeToolStripMenuItem.Checked = !silentModeToolStripMenuItem.Checked;
+			// TODO - Enable Silent Mode (Does Text based output instead of tts.)
+		}
+
+		private void textModeToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			textModeToolStripMenuItem.Checked = !textModeToolStripMenuItem.Checked;
+			// TODO - Enable Text mode (inputs text entered in the application as speech recognition and disables speech recognition)
+		}
+
+		private void startAutomaticallyToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			startAutomaticallyToolStripMenuItem.Checked = !startAutomaticallyToolStripMenuItem.Checked;
+			// TODO - Makes application start automatically as a service.
+		}
+
+
+		private void startJAPAToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			if (_JAPAenabled == false)
+			{
+				startJAPAToolStripMenuItem.Text = "Stop JAPA";
+				statusBar.Text = "Starting";
+				logger.Info("Starting JAPA...");
+				_JAPArunning = true;
+				userInput.Enabled = true;
+				companion.isAcceptingUserInput = true;
+				logger.Info("Session started.");
+			}
+			else
+			{
+				startJAPAToolStripMenuItem.Text = "Start JAPA";
+				statusBar.Text = "Stopping";
+				logger.Info("Stopping JAPA...");
+				_JAPArunning = false;
+				userInput.Enabled = false;
+				logger.Info("Session ended.");
+			}
+			_JAPAenabled = !_JAPAenabled;
+		}
+
+		private void userInput_TextChanged(object sender, EventArgs e)
+		{
+			if (userInput.Text != "" && userInput.Text != null && _JAPArunning == true)
+			{
+				btn_submitCommand.Enabled = true;
+			}
+			else
+			{
+				btn_submitCommand.Enabled = false;
+			}
+		}
+
+		private void btn_submitCommand_Click(object sender, EventArgs e)
+		{
+			logger.Info("Sent Text - \"{0}\"", userInput.Text);
+			Request r;
+			Result res;
+			string returnValue = "";
+
+			r = new Request(userInput.Text, user, companion);
+			res = companion.Chat(r);
+			returnValue = res.Output;
+
+			logger.Info("Response - \"{0}\"", returnValue.ToString());
+			userInput.Text = "";
+		}
+
+		private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			this.Close();
+		}
+
+		private void startSpeechRecognition()
+		{
+			recognitionEngine = new SpeechRecognitionEngine();
+
+			Choices commands = new Choices();
+			commands.Add(new string[] { "say hello", "print my name" });
+			GrammarBuilder gbuilder = new GrammarBuilder();
+			gbuilder.Append(commands);
+			Grammar grammar = new Grammar(gbuilder);
+
+			recognitionEngine.LoadGrammarAsync(grammar);
+			recognitionEngine.SetInputToDefaultAudioDevice();
+			recognitionEngine.SpeechRecognized += recognitionEngine_SpeechRecognized;
+			_JAPAenabled = true;
+
+			companion.isAcceptingUserInput = true;
+		}
+
+		private void recognitionEngine_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+		{
+			
+		}
+
+		protected override void OnResize(EventArgs e)
+		{
+			base.OnResize(e);
+
+			bool cursorNotInBar = Screen.GetWorkingArea(this).Contains(Cursor.Position);
+
+			if(this.WindowState == FormWindowState.Minimized && cursorNotInBar)
+			{
+				this.ShowInTaskbar = false;
+				notifyIcon.Visible = true;
+				this.Hide();
+				notifyIcon.ShowBalloonTip(500);
+
+			}
+		}
+
+		private void notifyIcon_MouseClick(object sender, MouseEventArgs e)
+		{
+			this.WindowState = FormWindowState.Normal;
+			this.ShowInTaskbar = true;
+			notifyIcon.Visible = false;
+			this.Visible = true;
+			this.Show();
+		}
+
+		private void tb_console_TextChanged(object sender, EventArgs e)
+		{
+			tb_console.SelectionStart = tb_console.TextLength;
+			tb_console.ScrollToCaret();
+		}
+	}
+}
